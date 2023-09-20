b5502db9f7d067e1d032d2bd4d467cf423b515c0
Nathalia T. Silva <nts.ef@outlook.com>
2023-09-20 08:59:04 -0300

    Iniciando o repositório

---
+++ README.md
@@ -0,0 +1,3 @@
+# FEA-dev USP: Python do Básico ao Intermediário
+
+## - Python Simplificado: Da Fundação ao Intermediário para Iniciantes
---
+++ aula02/aula02.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+2 + 2
+
+x = 5
+
+x * 2
+
+# Executando uma celula de jupityr dentro de um arquivo .py
+# %%
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4]
+y = [0, 1, 4, 9, 16]
+
+plt.plot(x, y)
+# %%
